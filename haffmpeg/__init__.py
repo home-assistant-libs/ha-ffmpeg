45c6b7e788e810ee1d1def626b9121614e11e143
Pascal Vizeli <pascal.vizeli@syshack.ch>
2016-08-06 17:53:37 +0200

    export logik from HA to ha-ffmpeg

--- haffmpeg/__init__.py
+++ haffmpeg/__init__.py
@@ -1,1 +1,5 @@
 """homeassistant ffmpeg shell wrapper."""
+from .core import HAFFmpeg, ITER_STDOUT, ITER_STDERR
+from .camera import CameraMjpeg
+
+__all__ = ['core', 'camera']
---
+++ haffmpeg/camera.py
@@ -0,0 +1,22 @@
+"""For HA camera components."""
+from .core import HAFFmpeg
+
+
+class CameraMjpeg(HAFFmpeg):
+    """Implement a camera they convert video stream to MJPEG."""
+
+    def __init__(self, ffmpeg_bin):
+        """Init CameraMjpeg."""
+        HAFFmpeg.__init__(self, ffmpeg_bin=ffmpeg_bin)
+
+    def open_camera(self, input_data, extra_cmd=None):
+        """Open FFmpeg process as mjpeg video stream."""
+        command = [
+            "-i",
+            input_data,
+            "-an",
+            "-c:v",
+            "mjpeg"
+        ]
+
+        self.open(cmd=command, extra_cmd=extra_cmd)
