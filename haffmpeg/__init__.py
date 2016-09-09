ff35e164e5c87bf43d97e9ecf840e3f299104141
Pascal Vizeli <pascal.vizeli@syshack.ch>
2016-09-09 21:16:55 +0200

    fix import

--- haffmpeg/__init__.py
+++ haffmpeg/__init__.py
@@ -2,6 +2,6 @@
 from .core import HAFFmpeg # NOQA
 from .camera import CameraMjpeg # NOQA
 from .sensor import SensorNoise, SensorMotion # NOQA
-from .tools import ImageSingle, IMAGE_JPEG, IMAGE_PNG # NOQA
+from .tools import ImageSingle, ImageStream, IMAGE_JPEG, IMAGE_PNG # NOQA
 
 __all__ = ['core', 'camera', 'sensor', 'tools']
