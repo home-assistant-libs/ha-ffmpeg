ade80fba704834816217350143abc31079eeb7a7
Pascal Vizeli <pascal.vizeli@syshack.ch>
2016-09-09 21:13:19 +0200

    fix test script

--- test/tools_imagestream.py
+++ test/tools_imagestream.py
@@ -13,17 +13,18 @@ logging.basicConfig(level=logging.DEBUG)
 @click.option("--ffmpeg", "-f", default="ffmpeg", help="FFmpeg binary")
 @click.option("--source", "-s", help="Input file for ffmpeg")
 @click.option("--format_img", "-f", default=IMAGE_JPEG, help="Image output format")
+@click.option("--prafix", "-p", default='jpg', help="Image präfix")
 @click.option("--output", "-o", help="Output image path")
 @click.option("--extra", "-e", help="Extra ffmpeg command line arguments")
 @click.option("--interval", "-i", default=10, help="Extra ffmpeg command line arguments")
-def cli(ffmpeg, source, format_img, output, extra, interval):
+def cli(ffmpeg, source, format_img, prafix, output, extra, interval):
     """FFMPEG capture frame as image."""
     count = 0
 
     def _callback(image):
         count += 1
-        name = os.path.join(output, "image{0}.{1}".format(count, format_img))
-        with open(, "wb") as fh_img:
+        name = os.path.join(output, "image{0}.{1}".format(count, prafix))
+        with open(name, "wb") as fh_img:
             fh_img.write(image)
 
     stream = ImageStream(ffmpeg_bin=ffmpeg, callback=_callback)
