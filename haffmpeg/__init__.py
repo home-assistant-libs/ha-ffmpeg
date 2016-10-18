6c8de26c28675f80f06b17d2fdb5a48dcc5d3a14
Pascal Vizeli <pascal.vizeli@syshack.ch>
2016-10-18 23:49:46 +0200

    add old support for CameraMjpeg

--- haffmpeg/__init__.py
+++ haffmpeg/__init__.py
@@ -1,6 +1,6 @@
 """homeassistant ffmpeg shell wrapper."""
 from .core import HAFFmpeg, HAFFmpegAsync # NOQA
-from .camera import CameraMjpegAsync # NOQA
+from .camera import CameraMjpeg, CameraMjpegAsync # NOQA
 from .sensor import SensorNoise, SensorMotion # NOQA
 from .tools import ( # NOQA
     Test, TestAsync, ImageSingle, ImageSingleAsync, ImageStream,
--- haffmpeg/camera.py
+++ haffmpeg/camera.py
@@ -1,7 +1,23 @@
 """For HA camera components."""
 import asyncio
 
-from .core import HAFFmpegAsync
+from .core import HAFFmpeg, HAFFmpegAsync
+
+
+class CameraMjpeg(HAFFmpeg):
+    """Implement a camera they convert video stream to MJPEG."""
+
+    def open_camera(self, input_source, extra_cmd=None):
+        """Open FFmpeg process as mjpeg video stream."""
+        command = [
+            "-an",
+            "-c:v",
+            "mjpeg",
+        ]
+
+        self.open(
+            cmd=command, input_source=input_source, output="-f mpjpeg -",
+            extra_cmd=extra_cmd)
 
 
 class CameraMjpegAsync(HAFFmpegAsync):
