fffa1729bd9c2f2d1892c8bd638c7c2914a8575a
Pascal Vizeli <pascal.vizeli@syshack.ch>
2018-11-22 14:56:46 +0100

    Update tools.py

--- haffmpeg/tools.py
+++ haffmpeg/tools.py
@@ -12,48 +12,6 @@ IMAGE_JPEG = 'mjpeg'
 IMAGE_PNG = 'png'
 
 
-class Test(HAFFmpeg):
-    """Test a ffmpeg/mulimedia file/stream."""
-
-    @asyncio.coroutine
-    def run_test(self, input_source, timeout=15):
-        """Start a test and give a TRUE or FALSE."""
-        command = [
-            "-frames:v",
-            "1",
-            "-frames:a",
-            "1",
-        ]
-
-        # Run a short test with input
-        is_open = yield from self.open(
-            cmd=command, input_source=input_source, stderr_pipe=True,
-            output=None)
-
-        # error after open?
-        if not is_open:
-            return False
-
-        try:
-            with async_timeout.timeout(timeout, loop=self._loop):
-                out, error = yield from self._proc.communicate()
-
-        except (OSError, asyncio.TimeoutError, ValueError):
-            _LOGGER.warning("Timeout/Error reading test.")
-            self._proc.kill()
-            return False
-
-        # check error code
-        if self._proc.returncode == 0:
-            _LOGGER.debug("STD: %s / ERR: %s", out, error)
-            return True
-
-        # error state
-        _LOGGER.error("ReturnCode: %i / STD: %s / ERR: %s",
-                      self._proc.returncode, out, error)
-        return False
-
-
 class ImageFrame(HAFFmpeg):
     """Implement a single image caputre from a stream."""
 
