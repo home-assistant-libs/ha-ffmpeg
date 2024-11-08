5c3fd210147ae9112ba27c4cd23803fc3baa7fd6
Martin Hjelmare <marhje52@gmail.com>
2024-11-08 14:28:33 +0100

    Bump version to 3.2.2

--- setup.py
+++ setup.py
@@ -1,38 +1,36 @@
 from setuptools import setup
 
-with open('README.md') as f:
+with open("README.md") as f:
     long_description = f.read()
 
-VERSION = "3.2.1"
+VERSION = "3.2.2"
 
 setup(
-    name='ha-ffmpeg',
+    name="ha-ffmpeg",
     version=VERSION,
-    license='BSD License',
-    author='Pascal Vizeli',
-    author_email='pvizeli@syshack.ch',
-    url='https://github.com/pvizeli/ha-ffmpeg',
-    download_url='https://github.com/pvizeli/ha-ffmpeg/tarball/'+VERSION,
-    description=('A library that handling with ffmpeg for home-assistant'),
+    license="BSD License",
+    author="Pascal Vizeli",
+    author_email="pvizeli@syshack.ch",
+    url="https://github.com/pvizeli/ha-ffmpeg",
+    download_url="https://github.com/pvizeli/ha-ffmpeg/tarball/" + VERSION,
+    description=("A library that handling with ffmpeg for home-assistant"),
     long_description=long_description,
     classifiers=[
-        'Programming Language :: Python',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-        'Topic :: Scientific/Engineering :: Atmospheric Science',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: OS Independent',
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Scientific/Engineering :: Atmospheric Science",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
     ],
-    keywords=['ffmpeg', 'homeassistant', 'wrapper', 'api'],
+    keywords=["ffmpeg", "homeassistant", "wrapper", "api"],
     zip_safe=False,
-    platforms='any',
-    packages=['haffmpeg'],
+    platforms="any",
+    packages=["haffmpeg"],
     include_package_data=True,
-    install_requires=[
-        'async_timeout'
-    ]
+    install_requires=["async_timeout"],
 )
